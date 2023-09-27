669fd118b98670fd5ceb2e2d4a65eba0f9585def
Wilson Maximo <wilson-max@hotmail.com>
2023-09-27 17:31:05 -0300

    Correção

--- src/migrations/4-posts_categories.js
+++ src/migrations/4-posts_categories.js
@@ -2,14 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
     await queryInterface.createTable('posts_categories', {
-  
       post_id: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -30,11 +23,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('posts_categories')
   }
 };
